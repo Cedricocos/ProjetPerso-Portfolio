763d4af226eae667e2165a6a9837ebf75c53b24c
Cédric <cedric.olivry@sfr.fr>
2021-02-22 17:20:05 +0100

    Ajout liens projets

--- JS/scroll.js
+++ JS/scroll.js
@@ -3,19 +3,19 @@ window.onscroll = function () {
     const link1 = document.getElementById("link1");
     const link2 = document.getElementById("link2");
     const link3 = document.getElementById("link3");
-    const link4 = document.getElementById("link4");
+    // const link4 = document.getElementById("link4");
 
     if (window.pageYOffset > 400) {
         header.classList.add("sticky", "header-scroll");
         link1.classList.add("text-dark");
         link2.classList.add("text-dark");
         link3.classList.add("text-dark");
-        link4.classList.add("text-dark");
+        // link4.classList.add("text-dark");
     } else {
         header.classList.remove("sticky", "header-scroll");
         link1.classList.remove("text-dark");
         link2.classList.remove("text-dark");
         link3.classList.remove("text-dark");
-        link4.classList.remove("text-dark");
+        // link4.classList.remove("text-dark");
     }
 };
